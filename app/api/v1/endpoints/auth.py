bb5b1e618a635cffd5e2f34565ebdb33f552013d
fayzullayevvvv <ozodbekfayzullayev1220@gmail.com>
2026-04-12 20:45:51 +0500

    updated

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,26 +1,34 @@
 from typing import Annotated
 
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, Body
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from sqlalchemy.orm import Session
 
-from app.schemas.auth import UserLoginResponse
+from app.schemas.auth import UserLoginResponse, RefreshRequest
 from app.core.deps import get_db
 from app.services.user_service import UserService
 
 
-router = APIRouter(
-    prefix="/auth",
-    tags=["Auth"]
-)
+router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
 @router.post("/login", response_model=UserLoginResponse, status_code=status.HTTP_200_OK)
 async def login_view(
     credentials: Annotated[HTTPBasicCredentials, Depends(HTTPBasic())],
-    db: Annotated[Session, Depends(get_db)]
+    db: Annotated[Session, Depends(get_db)],
 ):
     user_service = UserService(db)
     login_response = user_service.authenticate_user(credentials)
 
     return login_response
+
+
+@router.post(
+    "/refresh", response_model=UserLoginResponse, status_code=status.HTTP_200_OK
+)
+async def refresh_view(
+    data: Annotated[RefreshRequest, Body()], db: Annotated[Session, Depends(get_db)]
+):
+    user_service = UserService(db)
+    refresh_response = user_service.refresh_access_token(data.refresh_token)
+    return refresh_response
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import Session
 
 from app.repository.user_repo import UserRepo
 from app.schemas.auth import UserLoginResponse
-from app.core.security import generate_token, generate_refresh_token
+from app.core.security import (
+    generate_token,
+    generate_refresh_token,
+    verify_refresh_token,
+)
 
 
 class UserService:
@@ -18,12 +22,30 @@ class UserService:
         if not user or user.password != credentials.password:
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid username or password"
+                detail="Invalid username or password",
             )
-        
+
         token_data = {"sub": f"{user.id}"}
 
         return UserLoginResponse(
             access_token=generate_token(token_data),
-            refresh_token=generate_refresh_token(token_data)
+            refresh_token=generate_refresh_token(token_data),
+        )
+
+    def refresh_access_token(self, refresh_token: str) -> UserLoginResponse:
+        payload = verify_refresh_token(refresh_token)
+
+        user_id = payload.get("sub")
+
+        user = self.repo.get_user_by_id(user_id)
+
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+            )
+
+        new_access_token = generate_token({"sub": f"{user.id}"})
+
+        return UserLoginResponse(
+            access_token=new_access_token, refresh_token=refresh_token
         )
